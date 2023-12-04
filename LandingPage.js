0e69ba59d94a0d22514de1016f54ea09fe08ee25
Milan Jobanputra <107670121+Milan-Jobanputra@users.noreply.github.com>
2023-12-04 18:15:45 +0530

    Add files via upload

---
+++ LandingPage.js
@@ -0,0 +1,14 @@
+// src/components/LandingPage.js
+import React from 'react';
+import HeroSection from './HeroSection';
+
+function LandingPage() {
+  return (
+    <div>
+      <HeroSection />
+      {/* Add more sections and components as needed */}
+    </div>
+  );
+}
+
+export default LandingPage;
