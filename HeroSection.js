e5bb584f1af0c9e3f6db05c19a893085b424b9a4
Milan Jobanputra <107670121+Milan-Jobanputra@users.noreply.github.com>
2023-12-05 18:20:08 +0530

    Add files via upload

---
+++ HeroSection.js
@@ -0,0 +1,16 @@
+// src/components/HeroSection.js
+import React from 'react';
+
+function HeroSection() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Your Hero Product</h1>
+        <p>Discover the amazing features of our hero product.</p>
+        <button className="cta-button">Learn More</button>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
